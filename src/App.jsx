e6ab1995303befba18f03fa5746d21d660f5a296
ramarimei <56565699+ramarimei@users.noreply.github.com>
2020-04-23 10:41:37 +1200

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import Card from './components/Pinotnoir/pinot-noir';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Homepage from './components/Homepage';
+import PropsPage from './topics/1-props/PropsPage';
+import ArrayPage from './topics/2-arrays/ArrayPage';
+import ChildrenPage from './topics/3-children/ChildrenPage';
+import StatePage from './topics/4-state/StatePage';
+import FormsPage from './topics/5-forms/FormsPage';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <Router>
+        <Navbar />
 
+        <Switch>
+          <Route path="/props">
+            <PropsPage />
+          </Route>
+          <Route path="/arrays">
+            <ArrayPage />
+          </Route>
+          <Route path="/children">
+            <ChildrenPage />
+          </Route>
+          <Route path="/state">
+            <StatePage />
+          </Route>
+          <Route path="/forms">
+            <FormsPage />
+          </Route>
+          <Route path="/">
+            <Homepage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Homepage.css';
+import Products from '/Data/products.json';
 
 const Homepage = () => {
   return (
