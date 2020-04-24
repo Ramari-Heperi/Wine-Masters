44a3b329fb035bd55aef9febfba56080ff549422
ramarimei <56565699+ramarimei@users.noreply.github.com>
2020-04-24 23:55:51 +1200

    trying to style

--- src/components/NavBar2.jsx
+++ src/components/NavBar2.jsx
@@ -1,14 +1,14 @@
-/* eslint-disable */
-import React from 'react';
-import './NavBar.css';
-import Title from '../../../topics/1-props/components/Title';
+// /* eslint-disable */
+// import React from 'react';
+// import './NavBar.css';
+// import Title from '../../../topics/1-props/components/Title';
 
-const NavBar = (props) => {
-    return (
-        <div className={NavBar}>
-            {props.children}
-        </div>
-    );
-};
+// const NavBar = (props) => {
+//     return (
+//         <div className={NavBar}>
+//             {props.children}
+//         </div>
+//     );
+// };
 
-export default NavBar;
+// export default NavBar;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,28 +4,54 @@ import './Navbar.css';
 
 const Navbar = () => {
   return (
+
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Website!</a>
+      <a className="navbar-brand" href="#">MASTERS</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
 
       <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            SHOP
+        </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">ALL WINE</a>
+            <a class="dropdown-item" href="#">ROSE</a>
+            <a class="dropdown-item" href="#">RED WINE</a>
+            <a class="dropdown-item" href="#">SPARKLING WINE</a>
+            <a class="dropdown-item" href="#">RTD'S</a>
+            <a class="dropdown-item" href="#">GIFT VOUCHERS</a>
+          </div>
 
+        </li>
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
           <li className="nav-item active">
             {/* <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a> */}
             <NavLink exact to="/" activeClassName="" className="nav-link">
-              Home
+              WINE CLUB
             </NavLink>
           </li>
           <li className="nav-item">
             {/* <a className="nav-link" href="/about">About</a> */}
             <NavLink exact to="/about" activeClassName="" className="nav-link">
-              About
+              ABOUT
             </NavLink>
           </li>
+          <li className="nav-item">
+            {/* <a className="nav-link" href="/about">About</a> */}
+            <NavLink exact to="/about" activeClassName="" className="nav-link">
+              STOCKISTS
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            {/* <a className="nav-link" href="/about">About</a> */}
+            <NavLink exact to="/about" activeClassName="" className="nav-link">
+              ACCOUNT
+            </NavLink>
 
+          </li>
         </ul>
         <form className="form-inline my-2 my-lg-0">
           <input className="form-control mr-sm-2" type="search" placeholder="Search" />
@@ -34,6 +60,6 @@ const Navbar = () => {
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
